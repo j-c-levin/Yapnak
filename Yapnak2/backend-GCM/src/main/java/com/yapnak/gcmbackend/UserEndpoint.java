e82d2b29dd3e7f51248c897747601cd20498193d
joshua levin <joshua.c.levin@gmail.com>
2015-09-08 12:11:14 +0100

    Quick save before deploying

--- Yapnak2/backend-GCM/src/main/java/com/yapnak/gcmbackend/UserEndpoint.java
+++ Yapnak2/backend-GCM/src/main/java/com/yapnak/gcmbackend/UserEndpoint.java
@@ -3,6 +3,11 @@ package com.yapnak.gcmbackend;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.images.ImagesService;
+import com.google.appengine.api.images.ImagesServiceFactory;
+import com.google.appengine.api.images.ImagesServiceFailureException;
+import com.google.appengine.api.images.ServingUrlOptions;
 import com.google.appengine.api.utils.SystemProperty;
 
 import java.security.MessageDigest;
@@ -89,6 +94,64 @@ public class UserEndpoint {
         int randomNum = rand.nextInt((max - min) + 1) + min;
 
         return randomNum;
+    }
+
+    @ApiMethod(
+            name = "updatePhotoUrl",
+            path = "updatePhotoUrl",
+            httpMethod = ApiMethod.HttpMethod.POST)
+    public OfferEntity updatePhotoUrl() {
+        OfferEntity response = new OfferEntity();
+        Connection connection;
+        try {
+            if (SystemProperty.environment.value() ==
+                    SystemProperty.Environment.Value.Production) {
+                // Load the class that provides the new "jdbc:google:mysql://" prefix.
+                Class.forName("com.mysql.jdbc.GoogleDriver");
+                connection = DriverManager.getConnection("jdbc:google:mysql://yapnak-app:yapnak-main/yapnak_main?user=root");
+            } else {
+                // Local MySQL instance to use during development.
+                Class.forName("com.mysql.jdbc.Driver");
+                connection = DriverManager.getConnection("jdbc:mysql://173.194.230.210/yapnak_main", "client", "g7lFVLRzYdJoWXc3");
+            }
+            queryBlock:
+            try {
+                String query = "SELECT clientPhoto, clientID from client";
+                PreparedStatement statement = connection.prepareStatement(query);
+                ResultSet rs = statement.executeQuery();
+                query = "REPLACE client (clientPhotoUrl) VALUE (?) where clientID = ?";
+                statement = connection.prepareStatement(query);
+                while (rs.next()) {
+                    logger.info("found " + rs.getString("clientID"));
+                    ImagesService services = ImagesServiceFactory.getImagesService();
+                    ServingUrlOptions serve = ServingUrlOptions.Builder.withBlobKey(new BlobKey(rs.getString("clientPhoto")));
+                    String url;
+                    try {
+                        url = services.getServingUrl(serve);
+                        url = url + "=s100";
+                    } catch (IllegalArgumentException e) {
+                        url = "http://yapnak.com/images/yapnakmonster.png";
+                        e.printStackTrace();
+                    } catch (ImagesServiceFailureException e1) {
+                        url = "http://yapnak.com/images/yapnakmonster.png";
+                        e1.printStackTrace();
+                    }
+                    statement.setString(1, url);
+                    statement.setInt(2, rs.getInt("clientID"));
+                    int success = statement.executeUpdate();
+                    if (success == -1) {
+                        logger.warning("Failed to update logo for " + rs.getInt("clientID"));
+                    } else {
+                        logger.info(rs.getInt("clientID") + " logo updated");
+                    }
+                }
+            } finally {
+                connection.close();
+                return response;
+            }
+        } finally {
+            return response;
+        }
     }
 
     @ApiMethod(
@@ -517,7 +580,7 @@ public class UserEndpoint {
             }
             queryBlock:
             try {
-                String statement = "SELECT clientName,clientX,clientY,clientFoodStyle,clientPhoto,client.clientID,offers.offerText offer,offers.offerID FROM client JOIN offers ON client.clientID=offers.clientID AND offers.isActive = 1 AND offers.showOffer = 1 WHERE clientX BETWEEN ? AND ? AND clientY BETWEEN ? AND ?";
+                String statement = "SELECT clientName,clientX,clientY,clientFoodStyle,clientPhotoUrl,client.clientID,offers.offerText offer,offers.offerID FROM client JOIN offers ON client.clientID=offers.clientID AND offers.isActive = 1 AND offers.showOffer = 1 WHERE clientX BETWEEN ? AND ? AND clientY BETWEEN ? AND ?";
                 PreparedStatement stmt = connection.prepareStatement(statement);
                 double t = longitude - distance;
                 stmt.setDouble(1, t);
@@ -534,13 +597,14 @@ public class UserEndpoint {
                     while (rs.next()) {
                         logger.info("Retrieving client: " + rs.getString("clientName"));
                         offer = new OfferEntity();
+                        offer.setOfferId(rs.getInt("offerID"));
                         offer.setClientId(rs.getInt("clientID"));
                         offer.setClientName(rs.getString("clientName"));
                         offer.setOfferText(rs.getString("offer"));
                         offer.setLongitude(rs.getDouble("clientX"));
                         offer.setLatitude(rs.getDouble("clientY"));
                         offer.setFoodStyle(rs.getString("clientFoodStyle"));
-                        offer.setClientPhoto(rs.getString("clientPhoto"));
+                        offer.setClientPhoto(rs.getString("clientPhotoUrl"));
                         list.add(offer);
                     }
                     //sort list here by top three priority and then distance
@@ -585,7 +649,7 @@ public class UserEndpoint {
             }
             queryBlock:
             try {
-                String query = "SELECT clientName,clientX,clientY,clientFoodStyle,clientPhoto,client.clientID,offers.offerText offer,offers.offerID FROM client JOIN offers ON client.clientID=offers.clientID AND offers.isActive = 1 AND offers.showOffer = 1 WHERE clientName LIKE ?";
+                String query = "SELECT clientName,clientX,clientY,clientFoodStyle,clientPhotoUrl,client.clientID,offers.offerText offer,offers.offerID FROM client JOIN offers ON client.clientID=offers.clientID AND offers.isActive = 1 AND offers.showOffer = 1 WHERE clientName LIKE ?";
                 PreparedStatement statement = connection.prepareStatement(query);
                 statement.setString(1, "%" + searchString + "%");
                 logger.info("Searching for clients similar to: " + searchString);
@@ -595,13 +659,14 @@ public class UserEndpoint {
                     while (rs.next()) {
                         logger.info("Retrieving client: " + rs.getString("clientName"));
                         offer = new OfferEntity();
+                        offer.setOfferId(rs.getInt("offerID"));
                         offer.setClientId(rs.getInt("clientID"));
                         offer.setClientName(rs.getString("clientName"));
                         offer.setOfferText(rs.getString("offer"));
                         offer.setLongitude(rs.getDouble("clientX"));
                         offer.setLatitude(rs.getDouble("clientY"));
                         offer.setFoodStyle(rs.getString("clientFoodStyle"));
-                        offer.setClientPhoto(rs.getString("clientPhoto"));
+                        offer.setClientPhoto(rs.getString("clientPhotoUrl"));
                         list.add(offer);
                     }
                     //sort list here by top three priority and then distance
