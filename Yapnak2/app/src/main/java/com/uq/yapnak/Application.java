3094495e4ae4ba42340532c5d63e5cccefa36c9c
vahizan <vahizanv@gmail.com>
2015-09-29 21:06:09 +0100

    Implemented Parse- frontend

--- Yapnak2/app/src/main/java/com/uq/yapnak/Application.java
+++ Yapnak2/app/src/main/java/com/uq/yapnak/Application.java
@@ -3,7 +3,10 @@ package com.uq.yapnak;
 import android.support.multidex.MultiDexApplication;
 import android.util.Log;
 
+import com.parse.FunctionCallback;
 import com.parse.Parse;
+import com.parse.ParseCloud;
+import com.parse.ParseException;
 import com.parse.ParseInstallation;
 
 import java.util.HashMap;
@@ -17,11 +20,22 @@ public class Application extends MultiDexApplication {
     public void onCreate() {
         super.onCreate();
 
-        Parse.initialize(this,getResources().getString(R.string.app_id),getResources().getString(R.string.key));
+        Parse.initialize(this, getResources().getString(R.string.app_id), getResources().getString(R.string.key));
         ParseInstallation.getCurrentInstallation().saveInBackground();
         Log.d("debug", "Beginning");
         HashMap<String, Object> map = new HashMap<>();
         map.put("message", "hello there, how are you?");
         map.put("recipientId", ParseInstallation.getCurrentInstallation().getInstallationId());
+        ParseCloud.callFunctionInBackground("pushToUser", map, new FunctionCallback<String>() {
+
+            @Override
+            public void done(String s, ParseException e) {
+
+                if(e==null){
+                     Log.d("debug",s);
+                }
+            }
+        });
+
     }
 }
