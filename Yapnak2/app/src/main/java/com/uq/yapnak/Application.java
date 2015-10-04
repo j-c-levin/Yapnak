5774cd7c18c0c7f028b082c0d2ef8d5f05dd5375
joshua levin <joshua.c.levin@gmail.com>
2015-10-04 18:43:07 +0100

    add parse

--- Yapnak2/app/src/main/java/com/uq/yapnak/Application.java
+++ Yapnak2/app/src/main/java/com/uq/yapnak/Application.java
@@ -16,18 +16,6 @@ public class Application extends MultiDexApplication {
 
         Parse.initialize(this, getResources().getString(R.string.app_id), getResources().getString(R.string.key));
         ParseInstallation.getCurrentInstallation().saveInBackground();
-//        Log.d("debug", "Beginning");
-//        HashMap<String, Object> map = new HashMap<>();
-//        map.put("message", "hello there, how are you?");
-//        map.put("recipientId", ParseInstallation.getCurrentInstallation().getInstallationId());
-//        ParseCloud.callFunctionInBackground("pushToUser", map, new FunctionCallback<String>() {
-//            @Override
-//            public void done(String s, ParseException e) {
-//                if (e == null) {
-//                    Log.d("debug", s);
-//                }
-//            }
-//        });
 
     }
 }
